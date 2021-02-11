7b589f8233db4a0ab181c742ab6b2f79b1ab7e08
TrevorMicek <miceking@comcast.net>
2021-02-11 15:47:23 -0700

    commit on 'main'

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -2,16 +2,26 @@ import React from 'react';
 import styles from '../styles/SecondaryPages/mainStyles.module.css';
 const checkmark = require('../images/checkmark.jpg')
 
-const Benefits = (props) => (
-    <div>
+const Benefits = (props) => {
+  const benefit = () => {
+    return (
+      props.title === undefined ? null : 
+      <div>
     <div className={styles.imgWrapper}>
+     
         <img src={checkmark} className={styles.thirdGraphic} />
         </div>
-        <div className={styles.pricingWrapper}>
+    <div className={styles.pricingWrapper}>
         <h3 className={styles.benefitTitle}>
           {props.title}
         </h3>
         </div>
         </div>
-)
+    )
+        
+  }
+      return (
+        benefit()
+      )
+      }
 export default Benefits;
