b42adb79a37d1402977ee0ec841a004986a72d06
TrevorMicek <miceking@comcast.net>
2021-02-22 15:11:33 -0700

    commit on 'main'

--- src/components/aboutPage/aboutInfo.js
+++ src/components/aboutPage/aboutInfo.js
@@ -1,18 +1,18 @@
 const aboutInfo = {
     sectionOne : {
-        title: 'My Values',
-        firstP: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ',
-        secondP: "",
-        thirdP: ""
+        title: 'Business Goals',
+        firstP: "My main goal is to create the highest quality service you can find, that's also affordable to small businesses, and I'm well on my way.",
+        secondP: "I also see myself continuing to make more and more business partnerships to increase outreach, and help with scaling up.",
+        thirdP: "In 2-3 years, I see myself branching out to other countries such as Canada or EU members."
       },
       sectionTwo : {
-        title: 'My Tech Goals',
+        title: 'Tech Goals',
         firstP: "My main goal in tech is to constantly be learning new things because there's constantly new technologies, and additions to languages.",
-        secondP: "I want to build the best web dev service you can find, and I'm well on my way, but I also want to be able to use my knowledge of programming to help people somehow.",
+        secondP: "I also want to be able to use my knowledge of programming to help people somehow.",
         thirdP: "My favorite areas of IT, are web development, and AI, so I see myself utilizing both in the future."
       },
         sectionThree : {
-            title: 'My Life Goals',
+            title: 'Life Goals',
             firstP: "I'm eager to learn new things. I have a lot of goals in life that require a lot of research.",
             secondP: "Some of those goals involve helping others, like community outreach, charity, or meditation to make a difference.",
             thirdP: "Some of my goals are more for myself, like wanting to build my own house in the woods, or travelling, or learning a foreign language(czech)."
