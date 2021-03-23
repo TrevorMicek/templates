c115951d79d357a08ba8219d2b2d47e7d0ed7cfa
TrevorMicek <miceking@comcast.net>
2021-03-23 13:35:27 -0600

    commit on 'main'

--- src/storePage/redux/store.js
+++ src/storePage/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
 import * as reducers from "./ducks"
-import { window } from 'ssr-window'
+import { window, extend } from 'ssr-window'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+extend(window, {
+    compose: __REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+})
+const composeEnhancers = window.compose || compose
 const rootReducer = combineReducers(reducers)
 const enhancer = composeEnhancers(applyMiddleware(thunk))
 
